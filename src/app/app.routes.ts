5f508961716cb50e49a473e6f65cbf0ba181efca
Mohamed-Ahmed-devhub <mohamed.ahmed.programer@Gmail.com>
2026-09-10 02:05:05 +0300

    feat: add dynamic page titles for all routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,9 @@ import { Contact } from './components/contact/contact';
 import { Notfound } from './components/notfound/notfound';
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home, title: 'Home' },
-  { path: 'about', component: About, title: 'About' },
-  { path: 'portfolio', component: Portfolio, title: 'Portfolio' },
-  { path: 'contact', component: Contact, title: 'Contact' },
-  { path: '**', component: Notfound, title: '404 - Not Found' }
+  { path: 'home', component: Home, title: 'Home | Mohamed Ahmed' },
+  { path: 'about', component: About, title: 'About | Mohamed Ahmed' },
+  { path: 'portfolio', component: Portfolio, title: 'Portfolio | Mohamed Ahmed' },
+  { path: 'contact', component: Contact, title: 'Contact | Mohamed Ahmed' },
+  { path: '**', component: Notfound, title: '404 | Page Not Found' }
 ];
